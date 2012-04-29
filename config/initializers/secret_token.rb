b7049c482782809152246bf0cca9e7bdcbf75c71
Dan Relihan <drelihan@alum.mit.edu>
2012-04-29 16:24:14 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Investment::Application.config.secret_token = '386e9c386a8de7fa1fd373be959229c3e7d477927a2d102ea4ec73d548af1ccc5f2d8d64570a6562f548075a6075273a31affbb38bc8044f4028c95bae820f72'
